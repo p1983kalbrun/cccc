31d8e9e9a12ae7fcfbd81ed2693757e0430757a2
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2020-04-28 12:22:27 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/a/CcccController.java
@@ -0,0 +1,15 @@
+package com.a;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class CcccController {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
